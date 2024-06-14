848d8f6f2e99ca349a0d4246427c7b6e7a9aff60
Vineet Kumar <itsvineetkr@gmail.com>
2024-06-15 01:33:38 +0530

    authentication

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('', views.index),
+    path('home', views.home),
+    path('login', views.loginUser),
+    path('logout', views.loginUser),
+    path('signin', views.signin),
+
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.messages import constants as messages
 
-# Create your views here.
+
+
+
+def index(request):
+    return render(request, "login.html")
+
+def home(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request, "index.html")
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if username and email and password:
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+            user.save()
+            return redirect("/login")
+    return render(request, "signin.html")
+
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/home")
+        else:
+            return redirect("/login")
+    return render(request, "login.html")
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
+            
+
